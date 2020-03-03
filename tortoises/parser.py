9f3297aadd17e6ed6aa6863a7efe39a98abe12e7
LemuriaChen <dandanlemuria@gmail.com>
2020-03-03 22:55:22 +0800

    update parser.py

--- tortoises/parser.py
+++ tortoises/parser.py
@@ -1,10 +1,12 @@
 
 import requests
 import re
-import urllib
-
+import os
 from tortoises.agent import get_headers
 from tortoises.thread import ProcessWrapper
+import wget
+import time
+from urllib.error import HTTPError
 
 
 PDF_PATTERN = re.compile(r'(?:href|HREF)="?((?:http://)?.+?\.pdf)')
@@ -13,23 +15,20 @@ PDF_PATTERN = re.compile(r'(?:href|HREF)="?((?:http://)?.+?\.pdf)')
 url = 'http://web.stanford.edu/class/cs106b/'
 html = requests.get(url=url, headers=get_headers(random=True))
 
-tasks = [url + _ for _ in PDF_PATTERN.findall(html.text)]
+tasks = list(set([url + _ for _ in PDF_PATTERN.findall(html.text)]))
 
 
-def download_pdf(pdf_url, path=''):
-
-    file_name = pdf_url.split('/')[-1]
-    r = urllib.request.urlopen(url)
-    block_sz = 8192
-
-    with open(path + file_name, 'wb') as f:
-        while True:
-            buffer = r.read(block_sz)
-            if not buffer:
-                break
-            f.write(buffer)
-    print(f'saving < {file_name} > to path < {path} >')
+def download_pdf(pdf_url, path='./'):
+    print(pdf_url)
+    try:
+        file_name = pdf_url.split('/')[-1]
+        wget.download(pdf_url, out=os.path.join(path, file_name))
+    except HTTPError:
+        pass
 
 
+start = time.time()
 pw = ProcessWrapper(download_pdf, tasks, 10)
 pw.run()
+end = time.time()
+print(f'done, {round(end - start, 4)} seconds used.')
